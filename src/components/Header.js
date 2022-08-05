d463637cc4289bdc76531130a54e97c8db2c5579
luanfgoncalves <luanfgoncalves@outlook.com>
2022-08-05 12:35:02 -0300

    Feat: Req 2 completo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        <p>Header</p>
+        <div data-testid="email-field">
+          { email }
+        </div>
+        <div data-testid="total-field">
+          0
+        </div>
+        <div data-testid="header-currency-field">
+          campo de câmbio(BRL)
+        </div>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (globalState) => ({
+  email: globalState.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Header);

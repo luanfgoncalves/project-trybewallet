9756613f053fb30cc68930318bc31e3fa2967e0b
luanfgoncalves <luanfgoncalves@outlook.com>
2022-08-17 10:23:52 -0300

    Feat: Req 6 - completo

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react';
 export default class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        <th>Descrição</th>
+        <th>Tag</th>
+        <th>Método de pagamento</th>
+        <th>Valor</th>
+        <th>Moeda</th>
+        <th>Câmbio utilizado</th>
+        <th>Valor convertido</th>
+        <th>Moeda de conversão</th>
+        <th>Editar/Excluir</th>
+      </div>
     );
   }
 }
+
+// Referencia: https://www.w3schools.com/tags/tag_th.asp
